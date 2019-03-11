ee0193bf9c4eb68bf8c6d8550dc1873ef6ab6d67
JoshuaGale <joshua.gale@my.jcu.edu.au>
2019-03-11 15:33:14 +1000

    Add player rotation

--- Pirapelago/Assets/Scripts/PlayerController.cs
+++ Pirapelago/Assets/Scripts/PlayerController.cs
@@ -7,13 +7,20 @@ public class PlayerController : MonoBehaviour
     private Rigidbody2D rb2d;
     public int acceleration;
     public int maxSpeed;
+    public GameObject director;
+
+
+    Vector3 mousePositionInWorld;
+    float angle;
+    public float speedFector;
+    public float startRotationOffset;  //This is angle offset at starting.
+
     // Start is called before the first frame update
     void Start()
     {
         rb2d = GetComponent<Rigidbody2D>();     //assigns the rigidbody to the rb2d variable
     }
 
-    // Update is called once per frame
     void FixedUpdate()
     {
         //Takes user imput and adds forces to the player
@@ -23,5 +30,18 @@ public class PlayerController : MonoBehaviour
         rb2d.AddForce(movement * acceleration);
         rb2d.velocity = Vector2.ClampMagnitude(rb2d.velocity, maxSpeed);
     
+    }
+
+    void Update()
+    {
+            mousePositionInWorld = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            angle = (Mathf.Atan2(mousePositionInWorld.y - transform.position.y, mousePositionInWorld.x - transform.position.x) * Mathf.Rad2Deg);
+
+            transform.rotation = Quaternion.Lerp(transform.rotation, Quaternion.Euler(0, 0, angle + startRotationOffset), speedFector * Time.deltaTime);
+
+
+
+
+  
     }
 }
