56e5711b3a8788b508a345b269ab586615e56436
Thomas Napier <thomas.napier@my.jcu.edu.au>
2019-03-25 15:14:01 +1000

    Add UI trigger to shop island

---
+++ Pirapelago/Assets/Scripts/EnterShop.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class EnterShop : MonoBehaviour
+{
+    private GameObject shopUI;
+
+    private void Start()
+    {
+        shopUI = GameObject.FindGameObjectWithTag("shopUI");
+        shopUI.SetActive(false);
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        shopUI.SetActive(true);
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        shopUI.SetActive(false);
+    }
+}
