154ea4e8fcfa36ec8a22be6cba01c3557b747ada
Joshua Gale <joshua.gale@my.jcu.edu.au>
2019-04-14 16:35:16 +1000

    Add Enemy Spawner

---
+++ Pirapelago/Assets/Scripts/Other/EnemyManager.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyManager : MonoBehaviour
+{
+    public GameObject player;
+    public float spawnTime;
+    public Transform[] spawnPoints;
+    public GameObject enemy;
+    public bool spawned;
+    // Start is called before the first frame update
+    void Start()
+    {
+        InvokeRepeating("Spawn", spawnTime, spawnTime);
+    }
+
+    void Spawn()
+    {
+        if (player.GetComponent<CharacterStats>().CurrentHealth <= 0f)
+        {
+            return;
+        }
+        spawned = true;
+        int spawnPointIndex = Random.Range(0, spawnPoints.Length);
+        while (spawned)
+        {
+            spawnPointIndex = Random.Range(0, spawnPoints.Length);
+            if (spawnPoints[spawnPointIndex].gameObject.GetComponent<OverlapDetect>().collisions == 0)
+            {
+                Instantiate(enemy, spawnPoints[spawnPointIndex].position, spawnPoints[spawnPointIndex].rotation);
+                spawned = false;
+            }
+        }
+    }
+
+}
---
+++ Pirapelago/Assets/Scripts/Other/OverlapDetect.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OverlapDetect : MonoBehaviour
+{
+    public int collisions = 0;
+
+    void OnCollisionEnter(Collision collision)
+    {
+        collisions++;
+    }
+
+    void OnCollisionExit(Collision collision)
+    {
+        collisions--;
+    }
+}
